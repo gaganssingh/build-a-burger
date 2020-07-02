40862cc196def7d3e6be7fdc2d66627ae11bb451
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-02 19:35:05 -0400

    Added mobile styling and layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,35 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => (
-   <Aux>
-      <Toolbar />
-      <main className={classes.Content}>{props.children}</main>
-   </Aux>
-);
+class Layout extends React.Component {
+   state = {
+      showSideDrawer: false,
+   };
+
+   sideDrawerClosedHandler = () => {
+      this.setState({ showSideDrawer: false });
+   };
+
+   sideDrawerToggleHandler = () => {
+      this.setState((prevState) => {
+         return { showSideDrawer: !prevState.showSideDrawer };
+      });
+   };
+
+   render() {
+      return (
+         <Aux>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+            <SideDrawer
+               closed={this.sideDrawerClosedHandler}
+               open={this.state.showSideDrawer}
+            />
+            <main className={classes.Content}>{this.props.children}</main>
+         </Aux>
+      );
+   }
+}
 
 export default Layout;
